932c26a9881a88225278726685c2c32a74ee9db9
satej soman <satej.soman@gmail.com>
2019-07-08 00:08:06 -0500

    move face tracing to networkx implementation

--- prclz/topology/planar.py
+++ prclz/topology/planar.py
@@ -1,93 +1,74 @@
 from itertools import chain, combinations
-from typing import List, Optional, Sequence
+from typing import List, Sequence
 
-import networkx
+import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 from shapely.geometry import LineString, Point, Polygon
 
+from prclz.plotting import plot_polygons
+
 
 class Node(Point):
+    """ two-dimensional point container """
     def __hash__(self):
         return hash(tuple(map(lambda _:_[0], self.xy)))
+
+    def __repr__(self):
+        return "Node({}, {})".format(self.x, self.y)
+
+    def  __str__(self):
+        return self.__repr__()
 
 class Edge(LineString):
     """ undirected edge, with flags to indicate if the edge in on the interior, a road, or a barrier """
     def __init__(self, nodes: Sequence[Node]):
-        # since the edge is undirected, sort input points to speed up edge uniqueness calculations
-        nodes = sorted(nodes, key=lambda n: n.x)
+        # nodes = sorted(nodes, lambda p: (p.x, p.y))
         self.nodes = nodes
         self.interior = False
         self.road = False
         self.barrier = False
         super().__init__(coordinates=nodes)
 
-    def __len__(self):
+    def length(self):
         return self.nodes[0].distance(self.nodes[1])
+
+    def __str__(self):
+        return "Edge(({}, {})-({}, {}))".format(self.nodes[0].x, self.nodes[0].y, self.nodes[1].x, self.nodes[1].y)
     
     def __hash__(self):
-        pass
+        return hash(self.nodes[0]) + hash(self.nodes[1])
 
 class Face(Polygon):
     """ polygon defined by edges """
 
-class Graph(networkx.Graph):
-    def __init__(self, name: Optional[str] = "S", degree: int = 0, incoming_graph_data=None, **attr):
-        self.name = name 
-        self.degree = degree
+    def __init__(self, shell=None, holes=None, edges=None):
+        self.edges = edges
+        super().__init__(shell=shell, holes=holes)
+
+class PlanarGraph(nx.Graph):
+    def __init__(self, name: str = "S", dual_order: int = 0, incoming_graph_data=None, **attr):
+        attr["name"] = name
+        attr["dual_order"] = dual_order
         super().__init__(incoming_graph_data=incoming_graph_data, **attr)
+
+    def  __repr__(self):
+        return "{}{}".format(self.name, self.graph["dual_order"])
+
+    def  __str__(self):
+        return self.__repr__()
 
     def add_edge(self, edge: Edge, weight=None):
         assert isinstance(edge, Edge)
-        super().add_edge(edge.nodes[0], edge.nodes[1], spatial_edge=edge, weight=weight if weight else len(edge))
+        super().add_edge(edge.nodes[0], edge.nodes[1], planar_edge=edge, weight=weight if weight else edge.length())  
 
-    ## weak dual calculation functions
-    def get_embedding(self):
-        return {
-            node: sorted(
-                self[node], # get neighbors from adjacency dictionary  
-                key = lambda other, node=node: np.arctan2(other.x - node.x, other.y - node.y))
-            for node in self.nodes
-        }
-
+    ## weak dual calculation functions    
     def trace_faces(self):
-        """ algorithm for face-tracing taken from SAGE """
-        if len(self.nodes) < 2: 
-            return []
+        cycles = nx.algorithms.cycles.cycle_basis(self)
+        return [Face(shell=[(n.x, n.y) for n in cycle], edges=self.edges(cycle)) for cycle in cycles]
 
-        embedding = self.get_embedding()
-
-        # ensure we have unique edges by eliminating (b, a) if we've already seen (a, b)
-        edgeset = set(chain.from_iterable([(edge[0], edge[1]), (edge[1], edge[0])] for edge in self.edges))
-
-        # trace faces
-        faces : List[Face] = []
-
-        face = [edgeset.pop()]
-        while edgeset:
-            neighbors = embedding[face[-1][-1]]
-            next_node = neighbors[(neighbors.index(face[-1][-2])+1) % (len(neighbors))]
-            candidate_edge = (face[-1][-1], next_node)
-            if candidate_edge == face[0]: # loop complete
-                faces.append(face)
-                face = [edgeset.pop()]
-            else:
-                face.append(candidate_edge)
-                edgeset.remove(candidate_edge)
-        
-        # clean up any faces in progress when edgeset exhausted
-        if len(face) > 0:
-            faces.append(face)
-
-        # remove outer "sphere" face
-        facelist = sorted(faces, key=len)
-        self.outerface = Face(facelist[-1])
-        self.outerface.edges = [self[e[1]][e[0]]["spatial_edge"] for e in facelist[-1]]
-        
-        inner_facelist = [Face([self[e[1]][e[0]]["spatial_edge"] for e in edgelist]) for edgelist in facelist[:-1]]
-        return inner_facelist
-    
     def weak_dual(self):
-        dual = Graph(name = self.name, degree = self.degree + 1)
+        dual = PlanarGraph(name = self.name, dual_order = self.graph['dual_order'] + 1)
 
         if self.number_of_nodes() < 2: 
             return dual 
@@ -98,9 +79,25 @@ class Graph(networkx.Graph):
             dual.add_node(inner_facelist[0].centroid)
         else:
             for c in combinations(inner_facelist, 2):
-                c0 = [e for e in c[0].edges if not e.road]
-                c1 = [e for e in c[1].edges if not e.road]
+                c0 = [e for e in c[0].edges]
+                c1 = [e for e in c[1].edges]
                 if len(set(c0).intersection(c1)) > 0:
-                    dual.add_edge(Edge((c[0].centroid, c[1].centroid)))
+                    n1 = Node(c[0].centroid.x, c[0].centroid.y)
+                    n2 = Node(c[1].centroid.x, c[1].centroid.y)
+                    dual.add_edge(Edge((n1, n2)))
 
         return dual 
+
+    def plot(self, **kwargs):
+        plt.axes().set_aspect(aspect=1)
+        plt.axis('off')
+        edge_kwargs = kwargs.copy()
+        nlocs = {node: (node.x, node.y) for node in self.nodes}
+        edge_kwargs['label'] = "_nolegend"
+        edge_kwargs['pos'] = nlocs
+        nx.draw_networkx_edges(self, **edge_kwargs)
+        node_kwargs = kwargs.copy()
+        node_kwargs['label'] = self.name
+        node_kwargs['pos'] = nlocs
+        nodes = nx.draw_networkx_nodes(self, **node_kwargs)
+        nodes.set_edgecolor('None')
---
+++ smoketests/smoketest_graph.py
@@ -0,0 +1,50 @@
+from prclz.topology.planar import Edge, Face, Node, PlanarGraph
+import matplotlib.pyplot as plt
+
+
+n = [
+    None,
+    Node((0, 0)),
+    Node((0, 1)),
+    Node((0, 2)),
+    Node((0, 3)),
+    Node((1, 2)),
+    Node((1, 3)),
+    Node((0, 4)),
+    Node((-1, 4)),
+    Node((-1, 3)),
+    Node((-1, 2)),
+    Node((1, 4)),
+    Node((-2, 3))
+]
+
+s0 = PlanarGraph()
+s0.add_edge(Edge((n[1], n[2])))
+s0.add_edge(Edge((n[2], n[3])))
+s0.add_edge(Edge((n[2], n[5])))
+s0.add_edge(Edge((n[3], n[4])))
+s0.add_edge(Edge((n[3], n[5])))
+s0.add_edge(Edge((n[3], n[9])))
+s0.add_edge(Edge((n[4], n[5])))
+s0.add_edge(Edge((n[4], n[6])))
+s0.add_edge(Edge((n[4], n[7])))
+s0.add_edge(Edge((n[4], n[8])))
+s0.add_edge(Edge((n[4], n[9])))
+s0.add_edge(Edge((n[5], n[6])))
+s0.add_edge(Edge((n[6], n[7])))
+s0.add_edge(Edge((n[7], n[8])))
+s0.add_edge(Edge((n[8], n[9])))
+s0.add_edge(Edge((n[9], n[10])))
+s0.add_edge(Edge((n[3], n[10])))
+s0.add_edge(Edge((n[2], n[10])))
+s0.add_edge(Edge((n[7], n[11])))
+s0.add_edge(Edge((n[6], n[11])))
+s0.add_edge(Edge((n[10], n[12])))
+s0.add_edge(Edge((n[8], n[12])))
+
+plt.figure()
+
+s1 = s0.weak_dual()
+s0.plot(ax=plt.gca(), node_color='r', node_size=50)
+s1.plot(ax=plt.gca(), node_color='b', node_size=50)
+plt.show()
