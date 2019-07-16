fd7f5a0d15febc5f0779977ae1f0b3fb8012d1e8
satej soman <satej.soman@gmail.com>
2019-07-15 20:53:20 -0500

    add osm etl scripts

---
+++ osm-etl/extract.sh
@@ -0,0 +1,20 @@
+#!/bin/bash  
+
+set -eu 
+
+pbf_path="$1"
+output_prefix="$2"
+
+function extract() { 
+    output_name="${output_prefix}_${1}.geojson"
+    script="$2"
+    OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f GeoJSON ${output_prefix}_lines.geojson ${pbf_path} -sql "${script}"
+}
+
+extract "lines"             "select * from lines where natural = 'coastline' or highway is not null or waterway is not null"
+extract "buildings"         "select * from lines where building is not null"
+extract "building_polygons" "select * from multipolygons where building is not null"
+
+# OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f GeoJSON ${output_prefix}_lines.geojson             ${pbf_path} -sql "select * from lines where natural = 'coastline' or highway is not null or waterway is not null"
+# OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f GeoJSON ${output_prefix}_buildings.geojson         ${pbf_path} -sql "select * from lines where building is not null"
+# OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f GeoJSON ${output_prefix}_building_polygons.geojson ${pbf_path} -sql "select * from multipolygons where building is not null"
