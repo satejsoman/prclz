b8e1fdf6f0793af21aa1380be031bb1a13ca7b86
satej soman <satej.soman@gmail.com>
2019-07-17 12:22:49 -0500

    add midway check

--- osm-etl/extract.sh
+++ osm-etl/extract.sh
@@ -11,6 +11,12 @@ function extract() {
     OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f GeoJSON ${output_prefix}_lines.geojson ${pbf_path} -sql "${script}"
 }
 
+if [[ $(hostname) =~ ^midway* ]] ; then 
+    echo "loading modules on $(hostname)"
+    module load gdal/2.2
+    module load python/3.6.1+intel-16.0
+fi 
+
 extract "lines"             "select * from lines where natural = 'coastline' or highway is not null or waterway is not null"
 extract "buildings"         "select * from lines where building is not null"
 extract "building_polygons" "select * from multipolygons where building is not null"
