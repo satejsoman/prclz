10eb4a539bd19556d9f8f0c05bacbfabaeddd286
satej soman <satej.soman@gmail.com>
2019-07-31 15:49:13 -0500

    fix linter suggestions

--- midway/midway_complexity.py
+++ midway/midway_complexity.py
@@ -1,23 +1,23 @@
 import argparse
 import json
 import logging
-from logging import info, warning
+from logging import info
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import Optional
 
 import geopandas as gpd
 import pandas as pd
 import shapely.wkt
 from joblib import Parallel, delayed
 from networkx.readwrite import json_graph
-from shapely.geometry import MultiPolygon, Polygon
+from shapely.geometry import MultiPoint
 
-from prclz.blocks.methods import BufferedLineDifference
 from prclz.complexity import get_complexity, get_weak_dual_sequence
 
 
 def weak_dual_sequence_to_json_string(weak_duals):
     return json.dumps(list(map(json_graph.node_link_data, weak_duals)), default=lambda _:_.__dict__)
+
 
 def read_file(path, **kwargs):
     """ ensures geometry set correctly when reading from csv
@@ -33,15 +33,15 @@ def calculate_complexity(index, block, centroids):
     sequence = get_weak_dual_sequence(block, centroids)
     complexity = get_complexity(sequence)
     sequence_json = weak_dual_sequence_to_json_string(sequence)
-    centroids_multipoint = shapely.geometry.MultiPoint(centroids)
+    centroids_multipoint = MultiPoint(centroids)
 
     return (index, complexity, centroids_multipoint, sequence_json)
+
 
 def main(blocks_path: Path, buildings_path: Path, complexity_output: Path, graph_output: Optional[Path], parallelism: int):
     info("Reading geospatial data from files.")
     blocks    = read_file(str(blocks_path), index_col="block_id", usecols=["block_id", "geometry"], low_memory=False)
     buildings = read_file(str(buildings_path), low_memory=False)
-    
     buildings["geometry"] = buildings.centroid
 
     info("Aggregating buildings by street block.")
@@ -53,7 +53,7 @@ def main(blocks_path: Path, buildings_path: Path, complexity_output: Path, graph
 
     info("Calculating block complexity.")
     complexity = Parallel(n_jobs=parallelism, verbose=50)(delayed(calculate_complexity)(idx, block, centroids) for (idx, block, centroids) in block_buildings[["geometry", "centroids"]].itertuples())
-    block_buildings = block_buildings.join(pd.DataFrame(complexity, columns = ["block_id", "complexity", "centroids_multipoint", "weak_duals"]).set_index("block_id"))
+    block_buildings = block_buildings.join(pd.DataFrame(complexity, columns=["block_id", "complexity", "centroids_multipoint", "weak_duals"]).set_index("block_id"))
 
     info("Serializing complexity calculations to %s.", complexity_output)
     block_buildings[['geometry', 'complexity', 'centroids_multipoint']].to_csv(complexity_output)
@@ -61,13 +61,14 @@ def main(blocks_path: Path, buildings_path: Path, complexity_output: Path, graph
         info("Serializing graph sequences to %s", graph_output)
         block_buildings[['weak_duals']].to_csv(complexity_output)
 
+
 def setup(args=None):
     # logging
     logging.basicConfig(format="%(asctime)s/%(filename)s/%(funcName)s | %(levelname)s - %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
     logging.getLogger().setLevel("INFO")
 
     # read arguments
-    parser = argparse.ArgumentParser(description='Run parcelization workflow on midway2.')
+    parser = argparse.ArgumentParser(description='Run complexity workflow on midway2.')
     parser.add_argument('--blocks',      required=True,  type=Path, help='path to blocks',      dest="blocks_path")
     parser.add_argument('--buildings',   required=True,  type=Path, help='path to buildings',   dest="buildings_path")
     parser.add_argument('--output',      required=True,  type=Path, help='path to output',      dest="complexity_output")
