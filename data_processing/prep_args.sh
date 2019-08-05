1b129c8e0177003aafb48ea2dc8d14954cf0758d
Cooper Nederhood <cnederhood@midway2-login2.rcc.local>
2019-08-04 23:34:05 -0500

    Helpers for batch par for splitter

--- data_processing/prep_args.sh
+++ data_processing/prep_args.sh
@@ -8,10 +8,10 @@ mkdir tmp
 cur_wkr=0
 for f in ${target_dir}/*buildings.geojson ; do
     echo $f >> tmp/wkr${cur_wkr}.txt
-    echo "preupdate cur_wkr is $cur_wkr"
+    #echo "preupdate cur_wkr is $cur_wkr"
     ((cur_wkr++))    
     ((cur_wkr = cur_wkr % num_workers))
-    echo "cur_wkr is $cur_wkr"
+    #echo "cur_wkr is $cur_wkr"
 done
 
 
