98e84f788756f8df65649feeb65a77219a1c1252
satej soman <satej.soman@gmail.com>
2019-08-14 16:17:30 -0500

    fix spurious connections in planar graph construction by snapping input polygons to block

--- prclz/complexity.py
+++ prclz/complexity.py
@@ -11,16 +11,12 @@ def get_s0_approximation(block, centroids) -> PlanarGraph:
     """ approximates the initial connectivity graph by partitioning 
     the block into a voronoi decomposition and feeds those faces into
     a planar graph """
-    
-    # short circuit degenerate graphs
-    if len(centroids) < 3:
-        return PlanarGraph()
 
     # get the voronoi decomposition of space, given building centroids
     boundary_points = list(block.exterior.coords)
     boundary_set = set(boundary_points)
     vertices = [vs for (anchor, vs) in pytess.voronoi(centroids + boundary_points) if (anchor and anchor not in boundary_set)]
-    return PlanarGraph.from_polygons([Polygon(vs) for vs in vertices if len(vs) > 2])
+    return PlanarGraph.from_polygons([Polygon(vs).intersection(block) for vs in vertices if len(vs) > 2])
 
 def get_weak_dual_sequence_for_dataframe(
     gdf: gpd.GeoDataFrame, 
@@ -34,11 +30,10 @@ def get_weak_dual_sequence_for_dataframe(
 
 def get_weak_dual_sequence(block, centroids) -> Sequence[PlanarGraph]:
     s_vector = [get_s0_approximation(block, [(c.x, c.y) for c in centroids])]
-    while s_vector[-1].number_of_nodes() > 2:
+    while s_vector[-1].number_of_nodes() > 0:
         s_vector.append(s_vector[-1].weak_dual())
+    s_vector.pop() # last graph has no nodes
     return s_vector
 
 def get_complexity(sequence: Sequence[PlanarGraph]) -> int:
-    if not sequence:
-        return 0
-    return len(sequence) if sequence[-1].number_of_nodes() > 0 else len(sequence) - 1
+    return len(sequence) - 1 if sequence else 0
