6521cf6c9ac153ccb8ae88651901caf8265e371f
Cooper Nederhood <cnederhood@cs.uchicago.edu>
2019-08-16 14:28:45 -0500

    Fix helper

--- osm-etl/consolidate_buildings2.py
+++ osm-etl/consolidate_buildings2.py
@@ -17,9 +17,7 @@ so that it is easier (for me) to run over all the region-specific subfolders
 
 GEOJSON_PATH = "../data/geojson"
 
-all_files = glob.glob(GEOJSON_PATH + "/*/*building_polygons.geojson")
-
-def yield_main_args(all_polygon_files: str) -> (str, str, str):
+def process_all(all_polygon_files: str) -> (str, str, str):
     """
     Given a list of all the polygon building files, this uniquely identifies
     a country to process, so then provide the corrresponding 3 args
@@ -31,10 +29,10 @@ def yield_main_args(all_polygon_files: str) -> (str, str, str):
 
     for args in zip(all_polygon_files, all_linestrings_files, all_outputs_files):
 
-        main_process(*args)
+        process(*args)
 
 
-def main_process(polygons_path: str, linestrings_path: str, output: str):
+def process(polygons_path: str, linestrings_path: str, output: str):
     info("Unifying %s, %s", polygons_path, linestrings_path)
     polygons    = gpd.read_file(polygons_path)
     linestrings = gpd.read_file(linestrings_path)
@@ -46,19 +44,25 @@ def main_process(polygons_path: str, linestrings_path: str, output: str):
     concat[~concat.is_empty].to_file(output, driver='GeoJSON')
 
 
-def setup(args=None):
-    # logging
-    logging.basicConfig(format="%(asctime)s/%(filename)s/%(funcName)s | %(levelname)s - %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
-    logging.getLogger().setLevel("INFO")
+# def setup(args=None):
+#     # logging
+#     logging.basicConfig(format="%(asctime)s/%(filename)s/%(funcName)s | %(levelname)s - %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+#     logging.getLogger().setLevel("INFO")
 
-    # read arguments
-    parser = argparse.ArgumentParser(description='Consolidate building footprint geometries.')
-    parser.add_argument('--polygons',    required=True, type=Path, help='path to polygons',    dest="polygons_path")
-    parser.add_argument('--linestrings', required=True, type=Path, help='path to linestrings', dest="linestrings_path")
-    parser.add_argument('--output',      required=True, type=Path, help='path to output')
+#     # read arguments
+#     parser = argparse.ArgumentParser(description='Consolidate building footprint geometries.')
+#     parser.add_argument('--polygons',    required=True, type=Path, help='path to polygons',    dest="polygons_path")
+#     parser.add_argument('--linestrings', required=True, type=Path, help='path to linestrings', dest="linestrings_path")
+#     parser.add_argument('--output',      required=True, type=Path, help='path to output')
     
-    return parser.parse_args(args)
+#     return parser.parse_args(args)
 
 
 if __name__ == "__main__":
-    main(**vars(setup()))
+
+    logging.basicConfig(format="%(asctime)s/%(filename)s/%(funcName)s | %(levelname)s - %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+    logging.getLogger().setLevel("INFO")    
+
+    all_files = glob.glob(GEOJSON_PATH + "/*/*building_polygons.geojson")
+    process_all(all_files)
+
