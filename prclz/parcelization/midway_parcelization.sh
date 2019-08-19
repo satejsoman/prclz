1c06a13417200353bf7de24b5c00dddf52e2f482
Nicholas Marchio <namarchio@gmail.com>
2019-08-19 13:56:11 -0500

    Update midway_parcelization.sh

--- prclz/parcelization/midway_parcelization.sh
+++ prclz/parcelization/midway_parcelization.sh
@@ -7,9 +7,10 @@ module load udunits/2.2
 module load gdal/2.4.1 
 
 for countrycode in $(ls data/geojson_gadm/Africa/); do
-countryname=$(grep "$countrycode" midway/country_codes.geojson | rev | cut -d, -f2 | rev)
+countryname=$(grep "$countrycode" data_processing/country_codes.csv | rev | cut -d, -f2 | rev)
 mkdir -p data/parcels/Africa/${countrycode}
+mkdir -p prclz/parcelization/filled_templates
 echo "$countryname ($countrycode)"
-< midway/midway_parcelization.sbatch sed -e "s/::COUNTRYCODE::/${countrycode}/g" -e "s/::COUNTRYNAME::/${countryname}/g" > midway/filled_templates/${countrycode}_parcels.sbatch
-sbatch midway/filled_templates/${countrycode}_parcels.sbatch
+< prclz/parcelization/midway_parcelization.sbatch sed -e "s/::COUNTRYCODE::/${countrycode}/g" -e "s/::COUNTRYNAME::/${countryname}/g" > prclz/parcelization/filled_templates/${countrycode}_parcels.sbatch
+sbatch prclz/parcelization/filled_templates/${countrycode}_parcels.sbatch
 done
