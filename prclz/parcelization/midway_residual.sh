2ab2d69fa1e9fe663bfd76b801c7b6abf6337417
Nicholas Marchio <namarchio@gmail.com>
2019-08-23 00:07:06 -0500

    Update midway_residual.sh

--- prclz/parcelization/midway_residual.sh
+++ prclz/parcelization/midway_residual.sh
@@ -18,3 +18,23 @@ residual_list=(`echo ${building_list[@]} ${parcel_list[@]} | tr ' ' '\n' | sort 
 echo "${residual_list[@]}"
 
 printf '%s\n' "${residual_list[@]}"
+
+#!/bin/bash
+
+#SBATCH --job-name=PARCELS_::COUNTRYCODE::
+#SBATCH --partition=broadwl
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem=64000
+#SBATCH --output=logs/parcels_::COUNTRYCODE::.out
+#SBATCH --error=logs/parcels_::COUNTRYCODE::.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nmarchio@uchicago.edu
+#SBATCH --time=8:00:00
+#SBATCH --account=pi-bettencourt
+
+set -e
+
+for building in "${residual_list[@]}"; do
+    Rscript prclz/parcelization/midway_parcelization.R --building ${building};
+done
