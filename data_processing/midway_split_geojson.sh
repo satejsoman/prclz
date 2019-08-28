7f3bbd6471784aa21853775a7a0a42aa41b31a23
Cooper Nederhood <cnederhood@cs.uchicago.edu>
2019-08-28 12:14:49 -0500

    Midway scripts for building splitting

---
+++ data_processing/midway_split_geojson.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# module load R/3.6.1
+# module load udunits/2.2
+# module load gdal/2.4.1 
+
+for countryfile in $(ls ../data/geojson/*/*buildings*); do
+  file=$(echo $countryfile  | rev | cut -d/ -f1 | rev)
+  countryname=${file/_buildings.geojson/}
+  countryname=${countryname/_lines.geojson/}
+
+  type=${file/${countryname}/}
+  type=${type/_/}
+  type=${type/.geojson/}
+
+  countrycode=$(grep ${countryname} country_codes.csv | rev | cut -d, -f4 | rev)
+
+  echo file=$file
+  echo countryname=$countryname 
+  echo type=$type
+  echo countrycode=$countrycode
+  echo 
+
+  mkdir -p filled_templates
+  cat midway_split_geojson.sbatch | sed -e "s/::TYPE::/${type}/g" -e "s/::COUNTRYCODE::/${countrycode}/g" -e "s/::FILEPATH::/${file}/g"> filled_templates/${countrycode}_${type}.sbatch
+  #sbatch filled_templates/${countrycode}_parcels.sbatch
+done
