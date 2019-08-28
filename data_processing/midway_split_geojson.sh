3c87041846cd266a41c6cae6eb66d05fa865d521
Cooper Nederhood <cnederhood@cs.uchicago.edu>
2019-08-28 14:05:30 -0500

    Prepare for building split

--- data_processing/midway_split_geojson.sh
+++ data_processing/midway_split_geojson.sh
@@ -27,28 +27,7 @@ do
 
       mkdir -p filled_templates
       cat midway_split_geojson.sbatch | sed -e "s/::TYPE::/${type}/g" -e "s/::COUNTRYCODE::/${countrycode}/g" -e "s/::FILEPATH::/${file}/g"> filled_templates/${countrycode}_${type}.sbatch
+      sbatch filled_templates/${countrycode}_${type}.sbatch
   fi 
 
 done < $ALL_COUNTRIES
-
-
-
-# for countryfile in $(ls ../data/geojson/*/*buildings*); do
-#   file=$(echo $countryfile  | rev | cut -d/ -f1 | rev)
-#   countryname=${file/_buildings.geojson/}
-#   countryname=${countryname/_lines.geojson/}
-
-#   type=${file/${countryname}/}
-#   type=${type/_/}
-#   type=${type/.geojson/}
-
-#   echo file=$file
-#   echo countryname=$countryname 
-#   echo type=$type
-#   echo countrycode=$countrycode
-#   echo 
-
-#   mkdir -p filled_templates
-#   cat midway_split_geojson.sbatch | sed -e "s/::TYPE::/${type}/g" -e "s/::COUNTRYCODE::/${countrycode}/g" -e "s/::FILEPATH::/${file}/g"> filled_templates/${countrycode}_${type}.sbatch
-#   #sbatch filled_templates/${countrycode}_parcels.sbatch
-# done
