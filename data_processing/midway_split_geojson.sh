f11b1424fb0389a6f29690d48df18a33bf9af75f
Cooper Nederhood <cnederhood@cs.uchicago.edu>
2019-08-28 13:30:12 -0500

    Rework iteration

--- data_processing/midway_split_geojson.sh
+++ data_processing/midway_split_geojson.sh
@@ -6,46 +6,49 @@ set -e
 # module load udunits/2.2
 # module load gdal/2.4.1 
 
-for countryfile in $(ls ../data/geojson/*/*buildings*); do
-  file=$(echo $countryfile  | rev | cut -d/ -f1 | rev)
-  countryname=${file/_buildings.geojson/}
-  countryname=${countryname/_lines.geojson/}
+ALL_COUNTRIES="country_list.txt"
+type="buildings"     
+REGION="africa"
 
-  type=${file/${countryname}/}
-  type=${type/_/}
-  type=${type/.geojson/}
+while IFS= read -r countrycode
+do
+  countryname=$(grep "$countrycode" country_codes.csv | rev | cut -d, -f3 | rev)
+  region=$(grep "$countrycode" country_codes.csv | rev | cut -d, -f2 | rev)
+  file=${countryname}_${type}.geojson 
 
-  if [[ ${countryname} == "guinea" ]]
-  then 
-    countrycode="GIN"
+  if [[ ${region} == ${REGION} ]]
+  then
+      echo file=$file
+      echo countryname=$countryname 
+      echo region=$region 
+      echo type=$type
+      echo countrycode=$countrycode
+      echo 
 
-  elif [[ ${countryname} == "mali" ]]
-  then 
-    countrycode="MLI"
+      mkdir -p filled_templates
+      cat midway_split_geojson.sbatch | sed -e "s/::TYPE::/${type}/g" -e "s/::COUNTRYCODE::/${countrycode}/g" -e "s/::FILEPATH::/${file}/g"> filled_templates/${countrycode}_${type}.sbatch
+  fi 
 
-  elif [[ ${countryname} == "niger" ]]
-  then 
-    countrycode="NER"
+done < $ALL_COUNTRIES
 
-  elif [[ ${countryname} == "sudan" ]]
-  then 
-    countrycode="SDN"
 
-  elif [[ ${countryname} == "senegal-and-gambia" ]]
-  then 
-    countrycode="SEN"
-    
-  else
-    countrycode=$(grep ${countryname} country_codes.csv | rev | cut -d, -f4 | rev)
-  fi
 
-  echo file=$file
-  echo countryname=$countryname 
-  echo type=$type
-  echo countrycode=$countrycode
-  echo 
+# for countryfile in $(ls ../data/geojson/*/*buildings*); do
+#   file=$(echo $countryfile  | rev | cut -d/ -f1 | rev)
+#   countryname=${file/_buildings.geojson/}
+#   countryname=${countryname/_lines.geojson/}
 
-  mkdir -p filled_templates
-  cat midway_split_geojson.sbatch | sed -e "s/::TYPE::/${type}/g" -e "s/::COUNTRYCODE::/${countrycode}/g" -e "s/::FILEPATH::/${file}/g"> filled_templates/${countrycode}_${type}.sbatch
-  #sbatch filled_templates/${countrycode}_parcels.sbatch
-done
+#   type=${file/${countryname}/}
+#   type=${type/_/}
+#   type=${type/.geojson/}
+
+#   echo file=$file
+#   echo countryname=$countryname 
+#   echo type=$type
+#   echo countrycode=$countrycode
+#   echo 
+
+#   mkdir -p filled_templates
+#   cat midway_split_geojson.sbatch | sed -e "s/::TYPE::/${type}/g" -e "s/::COUNTRYCODE::/${countrycode}/g" -e "s/::FILEPATH::/${file}/g"> filled_templates/${countrycode}_${type}.sbatch
+#   #sbatch filled_templates/${countrycode}_parcels.sbatch
+# done
