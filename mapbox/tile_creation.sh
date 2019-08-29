1809ae482a3fb97bc92531978dff8f2f912a823f
Nicholas Marchio <namarchio@gmail.com>
2019-08-29 15:56:07 -0500

    Update tile_creation.sh

--- mapbox/tile_creation.sh
+++ mapbox/tile_creation.sh
@@ -1,27 +1,21 @@
 
-# Set directory
-cd /project2/bettencourt/mnp/prclz/data/mapbox_test
-
-# Create list of all geojson files in input directory
-complexity_list=()
-for i in /project2/bettencourt/mnp/prclz/data/mapbox_test/SLE_GEOJSON/*.geojson; do
-    complexity_list+=("$i")
-done
-echo "${complexity_list[@]}"
-mapbox_input=${complexity_list[@]}
-echo ${mapbox_input}
+# Concatenate files
+cat /project2/bettencourt/mnp/prclz/data/mapbox_test/SLE_GEOJSON/*.geojson > /project2/bettencourt/mnp/prclz/data/mapbox_test/all_gadms.geojson
 
 # Convert geojson to mbtile object at zoom level 1 object
-tippecanoe -o mapbox_output/sle_zoom1.mbtiles --force --exclude=centroids_multipoint  \
-	--minimum-zoom=0 --maximum-zoom=10 -P \
+tippecanoe -o /project2/bettencourt/mnp/prclz/data/mapbox_test/mapbox_output/sle_zoom1.mbtiles --force --exclude=centroids_multipoint --attribute-type=complexity:int \
+	--minimum-zoom=0 --maximum-zoom=10 -P  \
 	--coalesce-densest-as-needed --coalesce-smallest-as-needed --extend-zooms-if-still-dropping \
-	${mapbox_input}
+	/project2/bettencourt/mnp/prclz/data/mapbox_test/all_gadms.geojson
 
 # Convert geojson to mbtile object at zoom level 2 object
-tippecanoe -o mapbox_output/sle_zoom2.mbtiles --force --exclude=centroids_multipoint  \
+tippecanoe -o /project2/bettencourt/mnp/prclz/data/mapbox_test/mapbox_output/sle_zoom2.mbtiles --force --exclude=centroids_multipoint --attribute-type=complexity:int \
 	--minimum-zoom=10 --maximum-zoom=13 -P \
 	--coalesce-densest-as-needed --coalesce-smallest-as-needed --extend-zooms-if-still-dropping \
-	${mapbox_input}
+	/project2/bettencourt/mnp/prclz/data/mapbox_test/all_gadms.geojson
 
-# Join mbtiles together to allow differing zooms at different layers
-tile-join -o mapbox_output/sle_zoom_combined.mbtiles --force mapbox_output/sle_zoom1.mbtiles mapbox_output/sle_zoom2.mbtiles
+tile-join -o /project2/bettencourt/mnp/prclz/data/mapbox_test/mapbox_output/sle_zoom_combined.mbtiles --force /project2/bettencourt/mnp/prclz/data/mapbox_test/mapbox_output/sle_zoom1.mbtiles /project2/bettencourt/mnp/prclz/data/mapbox_test/mapbox_output/sle_zoom2.mbtiles
+
+
+scp nmarchio@midway.rcc.uchicago.edu:/project2/bettencourt/mnp/prclz/data/mapbox_test/mapbox_output/sle_zoom_combined.mbtiles /Users/nmarchio/Desktop
+
