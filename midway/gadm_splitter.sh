2e3cb4ea810dd755a28b1bef9b1e528be993524c
satej soman <satej.soman@gmail.com>
2019-09-03 13:51:31 -0500

    reference env variables correctly

--- midway/gadm_splitter.sh
+++ midway/gadm_splitter.sh
@@ -4,5 +4,5 @@ for country in $(find data/GADM -mindepth 1 -type d | grep "MEX\|CHN\|IRN\|IND\|
     split_path=${country/GADM/GADM_split}
     mkdir -p split_path
     gadm=$(ls ${country}/*.shp | python -c "import sys; print(sorted(sys.stdin.readlines())[-1].strip())")
-    python ./midway/gadm_splitter.py gadm split_path 
+    python ./midway/gadm_splitter.py ${gadm} ${split_path}
 done 
