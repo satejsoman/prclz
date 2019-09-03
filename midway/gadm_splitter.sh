d243777122ee1256c0f0d58c459007d7c60c4599
satej soman <satej.soman@gmail.com>
2019-09-03 13:52:22 -0500

    increase log output

--- midway/gadm_splitter.sh
+++ midway/gadm_splitter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for country in $(find data/GADM -mindepth 1 -type d | grep "MEX\|CHN\|IRN\|IND\|MYS\|PHL\|THA\|IDN\|BRA"); do 
+    echo ${country}
     split_path=${country/GADM/GADM_split}
     mkdir -p split_path
     gadm=$(ls ${country}/*.shp | python -c "import sys; print(sorted(sys.stdin.readlines())[-1].strip())")
