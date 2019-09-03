9e0166762dbd2800e47a759035bd852e996c757b
satej soman <satej.soman@gmail.com>
2019-09-03 13:47:55 -0500

    fix relative path in gadm splitter

--- midway/gadm_splitter.sh
+++ midway/gadm_splitter.sh
@@ -4,4 +4,4 @@ for country in $(find data/GADM -mindepth 1 -type d | grep "MEX\|CHN\|IRN\|IND\|
     split_path=${country/GADM/GADM_split}
     mkdir -p split_path
     gadm=$(ls data/GADM/${country_code}/*.shp | python -c "import sys; print(sorted(sys.stdin.readlines())[-1].strip())")
-    python ./gadm_splitter.py gadm split_path 
+    python ./midway/gadm_splitter.py gadm split_path 
