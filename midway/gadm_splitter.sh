817d804e37d766fd4b7ffd55368f25074eca275c
satej soman <satej.soman@gmail.com>
2019-09-03 13:49:22 -0500

    fix loop syntax

--- midway/gadm_splitter.sh
+++ midway/gadm_splitter.sh
@@ -5,3 +5,4 @@ for country in $(find data/GADM -mindepth 1 -type d | grep "MEX\|CHN\|IRN\|IND\|
     mkdir -p split_path
     gadm=$(ls data/GADM/${country_code}/*.shp | python -c "import sys; print(sorted(sys.stdin.readlines())[-1].strip())")
     python ./midway/gadm_splitter.py gadm split_path 
+done 
