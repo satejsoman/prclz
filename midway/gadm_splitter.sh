04195ecf8677a2a2b4a66b6d73bc6e8f04e2d65d
satej soman <satej.soman@gmail.com>
2019-09-03 13:57:37 -0500

    create gadm split directory

--- midway/gadm_splitter.sh
+++ midway/gadm_splitter.sh
@@ -3,7 +3,7 @@
 for country in $(find data/GADM -mindepth 1 -type d | grep "MEX\|CHN\|IRN\|IND\|MYS\|PHL\|THA\|IDN\|BRA"); do 
     echo ${country} | cut -d/ -f3
     split_path=${country/GADM/GADM_split}
-    mkdir -p split_path
+    mkdir -p ${split_path}
     gadm=$(ls ${country}/*.shp | python -c "import sys; print(sorted(sys.stdin.readlines())[-1].strip())")
     python ./midway/gadm_splitter.py ${gadm} ${split_path}
 done 
