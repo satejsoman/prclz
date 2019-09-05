ec15e8302a0970fc4ae18f42a3c3b60c34e0de6d
Nicholas Marchio <namarchio@gmail.com>
2019-09-05 12:48:37 -0500

    Update midway_parcelization_residual.sh

--- prclz/parcelization/midway_parcelization_residual.sh
+++ prclz/parcelization/midway_parcelization_residual.sh
@@ -35,6 +35,7 @@ country_list=(`echo ${residual_list[@]} | tr ' ' '\n' | sort | cut -d'/' -f 4 | 
 
 country_list=(LBR NPL HTI)
 
+# Submit a job for each residual country
 for countrycode in ${country_list[@]}; do
 	< prclz/parcelization/midway_parcelization.sbatch sed -e "s/::COUNTRYCODE::/${countrycode}/g" > prclz/parcelization/filled_templates/${countrycode}_parcels.sbatch
 	sbatch prclz/parcelization/filled_templates/${countrycode}_parcels.sbatch
