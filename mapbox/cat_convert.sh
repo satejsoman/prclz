60e40aac073b4bf79b462d07e7c7e0dc75a9b4e4
Nicholas Marchio <namarchio@gmail.com>
2019-09-12 15:14:00 -0500

    Update cat_convert.sh

--- mapbox/cat_convert.sh
+++ mapbox/cat_convert.sh
@@ -2,12 +2,12 @@
 module load gdal/2.4.1 
 
 # Concatenate CSVs
-head -1 /project2/bettencourt/mnp/prclz/mapbox/header.csv > /project2/bettencourt/mnp/prclz/data/tilesets/${TILESET_NAME}.csv
+head -1 /project2/bettencourt/mnp/prclz/mapbox/header.csv > /project2/bettencourt/mnp/prclz/data/tilesets/global_file.csv
 find /project2/bettencourt/mnp/prclz/data/complexity/*/*/*.csv -name "*.csv" | while read file
 do 
-    tail -n +2 -q $file >> /project2/bettencourt/mnp/prclz/data/tilesets/${TILESET_NAME}.csv 
+    tail -n +2 -q $file >> /project2/bettencourt/mnp/prclz/data/tilesets/global_file.csv 
     echo "$file"
 done
 
 # Convert CSV to GeoJSON
-ogr2ogr -f "GeoJSON" /project2/bettencourt/mnp/prclz/data/tilesets/${TILESET_NAME}.geojson /project2/bettencourt/mnp/prclz/data/tilesets/${TILESET_NAME}.csv 
+ogr2ogr -f "GeoJSON" /project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson /project2/bettencourt/mnp/prclz/data/tilesets/global_file.csv 
