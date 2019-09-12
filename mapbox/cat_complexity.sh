8f4a1728e99450fbb73e962e1e460438488258b0
Nicholas Marchio <namarchio@gmail.com>
2019-09-11 20:41:25 -0500

    Update cat_complexity.sh

--- mapbox/cat_complexity.sh
+++ mapbox/cat_complexity.sh
@@ -5,9 +5,6 @@ head -1 /project2/bettencourt/mnp/prclz/mapbox/header.csv > /project2/bettencour
 find /project2/bettencourt/mnp/prclz/data/complexity/${i}/*/*.csv -name "*.csv" | while read file
 do 
     tail -n +2 -q $file >> /project2/bettencourt/mnp/prclz/data/tilesets/${i}.csv 
+    echo "$file"
 done
 done
-
-module load intel/18.0
-module load gdal/2.2
-module load Anaconda3/5.1.0
