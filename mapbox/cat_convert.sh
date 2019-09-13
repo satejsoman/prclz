f96b7a9541b9795f8ddfa32292a4677095952d9e
Nicholas Marchio <namarchio@gmail.com>
2019-09-13 13:07:12 -0500

    Update cat_convert.sh

--- mapbox/cat_convert.sh
+++ mapbox/cat_convert.sh
@@ -7,5 +7,11 @@ do
     echo "$file"
 done
 
-# Convert CSV to GeoJSON-line delimited
-ogr2ogr -f GeoJSONSeq /project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson.ld /project2/bettencourt/mnp/prclz/data/tilesets/global_file.csv 
+# Convert CSV to GeoJSON
+module load R/3.6.1
+module load udunits/2.2
+module load gdal/2.4.1 
+Rscript /project2/bettencourt/mnp/prclz/mapbox/csv_to_geojson.R 
+
+# Convert GeoJSON to GeoJSON-line delimited
+ogr2ogr -f GeoJSONSeq /project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson.ld /project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson
