51f541441dab0937c92f12cba42f7dbe36c0b07f
Nicholas Marchio <namarchio@gmail.com>
2019-09-12 21:30:12 -0500

    Update tippecanoe_tileset.sh

--- mapbox/tippecanoe_tileset.sh
+++ mapbox/tippecanoe_tileset.sh
@@ -3,13 +3,13 @@
 tippecanoe -o zoom1.mbtiles --force --exclude=centroids_multipoint --attribute-type=complexity:int \
 	--minimum-zoom=0 --maximum-zoom=10 -P  \
 	--coalesce-densest-as-needed --coalesce-smallest-as-needed --extend-zooms-if-still-dropping \
-	${COMPLEXITY_GEOJSON_FILEPATH}
+	/project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson
 
 # Convert geojson to mbtile object at zoom level 2 object
 tippecanoe -o zoom2.mbtiles --force --exclude=centroids_multipoint --attribute-type=complexity:int \
 	--minimum-zoom=10 --maximum-zoom=13 -P \
 	--coalesce-densest-as-needed --coalesce-smallest-as-needed --extend-zooms-if-still-dropping \
-	${COMPLEXITY_GEOJSON_FILEPATH}
+	/project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson
 	
 # Join zoom levels
 tile-join -o zoom_combined.mbtiles --force zoom1.mbtiles zoom2.mbtiles
