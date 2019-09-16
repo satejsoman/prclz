5aade0de550671ab8a10abcb51866d5250aedd88
Nicholas Marchio <namarchio@gmail.com>
2019-09-16 14:49:46 -0500

    Update tippecanoe_tileset.sh

--- mapbox/tippecanoe_tileset.sh
+++ mapbox/tippecanoe_tileset.sh
@@ -2,17 +2,10 @@
 # Set destination directory
 cd /project2/bettencourt/mnp/prclz/data/tilesets
 
-# Convert geojson to mbtile object at zoom level 1 object
-tippecanoe -o zoom1.mbtiles --force --attribute-type=complexity:int \
-	--minimum-zoom=0 --maximum-zoom=10 -P  \
-	--coalesce-densest-as-needed --coalesce-smallest-as-needed --extend-zooms-if-still-dropping \
-	/project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson
+tippecanoe -o full_zoom.mbtiles --force --attribute-type=complexity:int \
+  --minimum-zoom=0 --maximum-zoom=13 -P \
+  --coalesce-smallest-as-needed \
+  /project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson
 
-# Convert geojson to mbtile object at zoom level 2 object
-tippecanoe -o zoom2.mbtiles --force --attribute-type=complexity:int \
-	--minimum-zoom=11 --maximum-zoom=13 -P \
-	--coalesce-densest-as-needed --coalesce-smallest-as-needed --extend-zooms-if-still-dropping \
-	/project2/bettencourt/mnp/prclz/data/tilesets/global_file.geojson
-	
 # Join zoom levels
-tile-join -o zoom_combined.mbtiles --force zoom1.mbtiles zoom2.mbtiles
+# tile-join -o zoom_combined.mbtiles --force zoom1.mbtiles zoom2.mbtiles
