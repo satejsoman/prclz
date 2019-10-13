f481fccdb3f23d22b7d36aa4bb6ce76a04e71b00
Nicholas Marchio <namarchio@gmail.com>
2019-10-13 18:19:37 -0500

    Update csv_to_geojson.sh

--- mapbox/prep/csv_to_geojson.sh
+++ mapbox/prep/csv_to_geojson.sh
@@ -1,6 +1,6 @@
 
 # Concatenate CSVs
-head -1 /project2/bettencourt/mnp/prclz/mapbox/header.csv > /project2/bettencourt/mnp/prclz/data/tilesets/global_file.csv
+head -1 /project2/bettencourt/mnp/prclz/mapbox/api/header.csv > /project2/bettencourt/mnp/prclz/data/tilesets/global_file.csv
 find /project2/bettencourt/mnp/prclz/data/complexity/*/*/*.csv -name "*.csv" | while read file
 do 
     tail -n +2 -q $file >> /project2/bettencourt/mnp/prclz/data/tilesets/global_file.csv 
