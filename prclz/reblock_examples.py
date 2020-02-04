dfcd1fccb9a2227416586081e151a42fed410716
Cooper Nederhood <cnederhood@cs.uchicago.edu>
2020-02-04 14:44:13 -0600

    Fix stuff

--- prclz/reblock_examples.py
+++ prclz/reblock_examples.py
@@ -1,13 +1,16 @@
 from i_reblock import reblock_gadm
 
 
-reblock_gadm(region, gadm_code, gadm, simplify, block_list=None, only_block_list=False, drop_already_completed=True)
+#reblock_gadm(region, gadm_code, gadm, simplify, block_list=None, only_block_list=False, drop_already_completed=True)
 
 
 def reblock_block_list(block_list, region):
 
 	gadm_list = [s[0:s.rfind("_")] for s in block_list]
 	gadm_code = gadm_list[0][0:3]
+	for gadm, block in zip(gadm_list, block_list):
+
+		reblock_gadm(region, gadm_code, [gadm], simplify, block_list=[block], only_block_list=True, drop_already_completed=True)
 
 
 nairobi = ['KEN.30.3.3_1_59', 'KEN.30.17.4_1_63', 'KEN.30.10.2_1_27', 'KEN.30.16.1_1_3']
@@ -19,5 +22,5 @@ block_aois = [nairobi, kathmandu, monrovia]
 
 for b, r in zip(block_aois, regions):
 
-	print("\n\n Working on {}-{}".format(b[0][0:3]))
+	print("\n\n Working on {}".format(b[0][0:3]))
 	reblock_block_list(b, r)
